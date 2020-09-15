5e0b73ae73ca0f13b9541e573aea184ae1543321
wooody92 <pkower4@naver.com>
2020-09-15 18:09:48 +0900

    init : 프로젝트 생성 및 기본 설정

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # java-practice
-Java 관련 학습 저장소
+
+Java에 대해 공부하고 학습한 내용을 블로그에 정리합니다.
+
+1. Effective Java 3/E
---
+++ src/main/java/chapter2/Item1.java
@@ -0,0 +1,8 @@
+package chapter2;
+
+public class Item1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Effective Java");
+    }
+}
