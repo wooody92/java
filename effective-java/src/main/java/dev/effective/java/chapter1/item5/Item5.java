37c79e9fe8571c02b57d6d1e7587eadfb1b59c94
wooody92 <pkower4@naver.com>
2020-09-19 15:17:03 +0900

    post : Item5 - 자원을 직접 명시하지 말고 의존 객체 주입을 사용하라

---
+++ effective-java/src/main/java/dev/effective/java/chapter1/item5/Item5.java
@@ -0,0 +1,9 @@
+package dev.effective.java.chapter1.item5;
+
+public class Item5 {
+
+    public static void main(String[] args) {
+//        LottoTicketGenerator generator = new LottoTicketGenerator();
+//        System.out.println(generator.generate());
+    }
+}
