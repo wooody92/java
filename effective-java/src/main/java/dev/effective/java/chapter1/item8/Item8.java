bb1e38dd9e968f54dfd854bba7ae01aeb3b6c71c
wooody92 <pkower4@naver.com>
2020-10-07 00:08:39 +0900

    post : Item9 - try-finally 보다는 try-with-resources를 사용하라

---
+++ effective-java/src/main/java/dev/effective/java/chapter1/item8/Item8.java
@@ -0,0 +1,13 @@
+package dev.effective.java.chapter1.item8;
+
+public class Item8 implements AutoCloseable{
+
+    @Override
+    public void close() throws Exception {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
