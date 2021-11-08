62a0640d4cbd9c5e20253e977aa25c186ff2a9ed
wooody92 <pkower4@gmail.com>
2021-11-08 19:21:57 +0900

    post : Add url-encode method on signature

--- 01. Cipher/src/main/java/sha256/Main.java
+++ 01. Cipher/src/main/java/sha256/Main.java
@@ -1,8 +1,10 @@
 package sha256;
 
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Base64;
 import rsa.RSAUtil;
 
 public class Main {
@@ -24,5 +26,19 @@ public class Main {
         // verify
         boolean verifiedResult = SHA256Util.verify(plainText, publicKey, signature);
         System.out.println("signature equality : " + verifiedResult);
+
+        // key handling
+        String privateKeyStr = keyToKeyStr(privateKey);
+        String publicKeyStr = keyToKeyStr(publicKey);
+
+        PrivateKey privateKey2 = RSAUtil.keyStrToPrivateKey(privateKeyStr);
+        PublicKey publicKey2 = RSAUtil.keyStrToPublicKey(publicKeyStr);
+
+        PublicKey extractedPublicKey = RSAUtil.extractPublicKey(privateKey);
+        System.out.println("public-key equality : " + publicKey.equals(extractedPublicKey));
+    }
+
+    private static String keyToKeyStr(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
--- 01. Cipher/src/main/java/sha256/SHA256Util.java
+++ 01. Cipher/src/main/java/sha256/SHA256Util.java
@@ -1,5 +1,7 @@
 package sha256;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -25,6 +27,26 @@ public class SHA256Util {
 
         byte[] clientDecodedSignature = Base64.getDecoder().decode(clientSignature.getBytes());
 
+        return signature.verify(clientDecodedSignature);
+    }
+
+    public static String signWithUrlEncoded(String plainText, PrivateKey privateKey) throws Exception {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(plainText.getBytes(StandardCharsets.UTF_8));
+
+        byte[] encodedSignature = Base64.getEncoder().encode(signature.sign());
+        String encodedSignatureStr = new String(encodedSignature, StandardCharsets.UTF_8);
+        return URLEncoder.encode(encodedSignatureStr, "UTF-8");
+    }
+
+    public static boolean verifyWithUrlDecoded(String plainText, PublicKey publicKey, String clientSignature) throws Exception {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initVerify(publicKey);
+        signature.update(plainText.getBytes(StandardCharsets.UTF_8));
+
+        String urlDecoded = URLDecoder.decode(clientSignature, "UTF-8");
+        byte[] clientDecodedSignature = Base64.getDecoder().decode(urlDecoded.getBytes());
         return signature.verify(clientDecodedSignature);
     }
 }
